e80e6e5d2407945535f9b0e15843ddd428c4bc6e
Marius Göcke <marius.goecke@gmail.com>
2022-01-24 21:09:03 +0100

    added some hsv-support for ExtendedColor

--- GRYLibrary/Miscellaneous/ExtendedColor.cs
+++ GRYLibrary/Miscellaneous/ExtendedColor.cs
@@ -1,4 +1,7 @@
 ﻿
+using System;
+using System.Drawing;
+
 namespace GRYLibrary.Core.Miscellaneous
 {
     public class ExtendedColor
@@ -12,28 +15,33 @@ namespace GRYLibrary.Core.Miscellaneous
         public byte R { get; }
         public byte G { get; }
         public byte B { get; }
-        public System.Drawing.Color DrawingColor { get; }
+        public Color DrawingColor { get; }
+        public (double hue, PercentValue saturation, PercentValue value) HSV { get; }
         public int ColorCode { get; }
 
         public ExtendedColor() : this(0)
         {
         }
-        public ExtendedColor(byte a, byte r, byte g, byte b) : this(System.Drawing.Color.FromArgb(a, r, g, b).ToArgb())
+        public ExtendedColor(double hue, PercentValue saturation, PercentValue value) : this(ColorFromHSV(hue, saturation, value).ToArgb())
         {
         }
-        public ExtendedColor(byte r, byte g, byte b) : this(System.Drawing.Color.FromArgb(r, g, b).ToArgb())
+        public ExtendedColor(byte a, byte r, byte g, byte b) : this(Color.FromArgb(a, r, g, b).ToArgb())
+        {
+        }
+        public ExtendedColor(byte r, byte g, byte b) : this(Color.FromArgb(r, g, b).ToArgb())
         {
         }
         public ExtendedColor(int colorCode)
         {
             this.ColorCode = colorCode;
-            this.DrawingColor = System.Drawing.Color.FromArgb(this.ColorCode);
+            this.DrawingColor = Color.FromArgb(this.ColorCode);
             this._ARGBStringValue = this.ColorCode.ToString("X8");
             this._RGBStringValue = this.DrawingColor.R.ToString("X2") + this.DrawingColor.G.ToString("X2") + this.DrawingColor.B.ToString("X2");
             this.A = this.DrawingColor.A;
             this.R = this.DrawingColor.R;
             this.G = this.DrawingColor.G;
             this.B = this.DrawingColor.B;
+            this.HSV = ColorToHSV(this.DrawingColor);
         }
 
         public string GetARGBString(bool withNumberSign = false)
@@ -77,6 +85,41 @@ namespace GRYLibrary.Core.Miscellaneous
         {
             return $"{nameof(ExtendedColor)}({nameof(this.A)}={this.A},{nameof(this.R)}={this.R},{nameof(this.G)}={this.G},{nameof(this.B)}={this.B})";
         }
+        private static (double hue, PercentValue saturation, PercentValue value) ColorToHSV(Color color)
+        {
+            int max = Math.Max(color.R, Math.Max(color.G, color.B));
+            int min = Math.Min(color.R, Math.Min(color.G, color.B));
 
+            double hue = color.GetHue();
+            double saturation = (max == 0) ? 0 : 1d - (1d * min / max);
+            double value = max / 255d;
+            return (hue, new PercentValue(saturation), new PercentValue(value));
+        }
+        private static Color ColorFromHSV(double hue, PercentValue saturation, PercentValue value)
+        {
+            double dSaturation = decimal.ToDouble(saturation.Value);
+            double dValue = decimal.ToDouble(value.Value);
+            int hi = Convert.ToInt32(Math.Floor(hue / 60)) % 6;
+            double f = hue / 60 - Math.Floor(hue / 60);
+
+            dValue = dValue * 255;
+            int v = Convert.ToInt32(dValue);
+            int p = Convert.ToInt32(dValue * (1 - dSaturation));
+            int q = Convert.ToInt32(dValue * (1 - f * dSaturation));
+            int t = Convert.ToInt32(dValue * (1 - (1 - f) * dSaturation));
+
+            if (hi == 0)
+                return Color.FromArgb(255, v, t, p);
+            else if (hi == 1)
+                return Color.FromArgb(255, q, v, p);
+            else if (hi == 2)
+                return Color.FromArgb(255, p, v, t);
+            else if (hi == 3)
+                return Color.FromArgb(255, p, q, v);
+            else if (hi == 4)
+                return Color.FromArgb(255, t, p, v);
+            else
+                return Color.FromArgb(255, v, p, q);
+        }
     }
 }
