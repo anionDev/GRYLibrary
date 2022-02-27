a3e563733a22c7c68711be24834b6ae44d89c2ec
Marius Göcke <marius.goecke@gmail.com>
2022-02-27 14:43:39 +0100

    added healthcheck helper for containerized .net applications

---
+++ GRYLibrary/Miscellaneous/Healthcheck/Healthcheck.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GRYLibrary.Core.Miscellaneous.Healthcheck
+{
+    public sealed class Healthcheck : IDisposable
+    {
+        public string File { get; }
+        public bool AddTimestamp { get; set; } = true;
+        public Encoding Encoding { get; set; } = new UTF8Encoding(false);
+        public Healthcheck(string file)
+        {
+            this.File = file;
+        }
+        public void SetState(HealthcheckValue value)
+        {
+            Utilities.EnsureFileExists(File);
+            string text;
+            if (AddTimestamp)
+            {
+                text = Utilities.DateTimeToISO8601String(DateTime.Now) + ": ";
+            }
+            else
+            {
+                text = string.Empty;
+            }
+            text = text + Enum.GetName(typeof(HealthcheckValue), value);
+            Utilities.AppendLineToFile(File, text, Encoding);
+        }
+        public void Dispose()
+        {
+            SetState(HealthcheckValue.NotRunning);
+        }
+    }
+}
---
+++ GRYLibrary/Miscellaneous/Healthcheck/HealthcheckValue.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GRYLibrary.Core.Miscellaneous.Healthcheck
+{
+    public enum HealthcheckValue
+    {
+        NotRunning = 0,
+        RunningHealthy = 1,
+        RunningUnhealthy = 2,
+    }
+}
