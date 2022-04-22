747a9ecfe6828f5c64e56a5664c1fd83f02df16a
Marius Göcke <marius.goecke@gmail.com>
2022-04-22 22:42:43 +0200

    updated run-testcases-script

--- InternalScripts/RunTestcases.py
+++ InternalScripts/RunTestcases.py
@@ -3,12 +3,11 @@ from pathlib import Path
 from ScriptCollection.ScriptCollectionCore import ScriptCollectionCore
 from ScriptCollection.GeneralUtilities import GeneralUtilities
 
-
 def run_testcases():
-    sc=ScriptCollectionCore()
-    repository_folder=str(Path(os.path.dirname(__file__)).parent.absolute())
-    sc.run_testcases_for_csharp_project(sc,repository_folder,"QualityCheck")
+    sc = ScriptCollectionCore()
+    repository_folder = str(Path(os.path.dirname(__file__)).parent.absolute())
+    sc.run_testcases_for_csharp_project(sc, repository_folder, "GRYLibraryTests", "QualityCheck")
     sc.generate_coverage_report(repository_folder)
 
-if __name__=="__main__":
+if __name__ == "__main__":
     run_testcases()
