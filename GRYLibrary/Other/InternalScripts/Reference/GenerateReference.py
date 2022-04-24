434f40006e28f5af9e1fabc5754b06949d131c21
Marius Göcke <marius.goecke@gmail.com>
2022-04-24 20:01:12 +0200

    updates for generating reference

--- GRYLibrary/Other/InternalScripts/Reference/GenerateReference.py
+++ GRYLibrary/Other/InternalScripts/Reference/GenerateReference.py
@@ -8,9 +8,10 @@ def standardized_tasks_generate_refefrence_for_dotnet_project_in_common_project_
     reference_result_folder=os.path.join(reference_folder,"Result")
     GeneralUtilities.ensure_directory_does_not_exist(reference_result_folder)
     self.run_program("docfx","docfx.json",reference_folder)
+    #TODO include coverage-report. it must also be specified for common-project-repositories that GenerateReference creates a .../Reference/ResultFolder which contains an index.html and the coverage-report.
 
 def generate_reference():
-    standardized_tasks_generate_refefrence_for_dotnet_project_in_common_project_structure(ScriptCollectionCore(),str(Path(os.path.dirname(__file__)).parent.absolute()), "GRYLibraryTests", "QualityCheck")
+    standardized_tasks_generate_refefrence_for_dotnet_project_in_common_project_structure(ScriptCollectionCore(),str(Path(os.path.dirname(__file__)).parent.parent.parent.parent.absolute()))
 
 if __name__ == "__main__":
     generate_reference()
--- ReadMe.md
+++ ReadMe.md
@@ -1,8 +1,7 @@
 ﻿# GRYLibrary
 
 [![CodeFactor](https://www.codefactor.io/repository/github/aniondev/grylibrary/badge/main)](https://www.codefactor.io/repository/github/aniondev/grylibrary/overview/main)
-![Coverage](./Other/Badges/TestCoverage/badge_shieldsio_linecoverage_blue.svg)
-![GitHub last commit](https://img.shields.io/github/last-commit/anionDev/GRYLibrary)
+![Coverage](./GRYLibrary//Other/TestCoverage/Badges/badge_shieldsio_linecoverage_blue.svg)
 ![Lines of code](https://img.shields.io/tokei/lines/github/anionDev/GRYLibrary)
 
 The GRYLibrary is a collection with some useful .NET classes and functions which are very easy (re)usable.
