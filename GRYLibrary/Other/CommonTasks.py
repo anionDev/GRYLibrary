831be31c56454a3b9428dcc436357b46f16c335e
Marius Göcke <marius.goecke@gmail.com>
2022-05-18 00:49:22 +0200

    updates due to new structure

---
+++ GRYLibrary/Other/CommonTasks.py
@@ -0,0 +1,3 @@
+#TODO update codeunit-version (set to project-version)
+#TODO update version in csproj
+#TODO update copyright-year and version in nuspec-file
--- GRYLibrary/Other/Reference/GenerateReference.py
+++ GRYLibrary/Other/Reference/GenerateReference.py
@@ -1,16 +1,9 @@
-import os
 from pathlib import Path
 from ScriptCollection.ScriptCollectionCore import ScriptCollectionCore
-from ScriptCollection.GeneralUtilities import GeneralUtilities
 
-def standardized_tasks_generate_refefrence_for_dotnet_project_in_common_project_structure(self:ScriptCollectionCore,repository:str):
-    reference_folder=os.path.join(repository,"GRYLibrary","Other","InternalScripts","Reference")
-    reference_result_folder=os.path.join(reference_folder,"Result")
-    GeneralUtilities.ensure_directory_does_not_exist(reference_result_folder)
-    self.run_program("docfx","docfx.json",reference_folder)
 
 def generate_reference():
-    standardized_tasks_generate_refefrence_for_dotnet_project_in_common_project_structure(ScriptCollectionCore(),str(Path(os.path.dirname(__file__)).parent.parent.parent.parent.absolute()))
+    ScriptCollectionCore().standardized_tasks_generate_reference_by_docfx(Path(__file__).absolute())
 
-if __name__ == "__main__":
-    generate_reference()
+
+generate_reference()
