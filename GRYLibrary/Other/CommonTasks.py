63942caa251b5c403b7ef3ba2d1c655f0e8461e6
Marius Göcke <marius.goecke@gmail.com>
2022-05-28 23:06:20 +0200

    fixes

--- GRYLibrary/Other/CommonTasks.py
+++ GRYLibrary/Other/CommonTasks.py
@@ -5,33 +5,37 @@ from pathlib import Path
 from ScriptCollection.ScriptCollectionCore import ScriptCollectionCore
 from ScriptCollection.GeneralUtilities import GeneralUtilities
 
-def replace_version_in_nuspec_file(self:ScriptCollectionCore,nuspec_file:str,current_version:str):
-        versionregex = "\\d+\\.\\d+\\.\\d+"
-        versiononlyregex = f"^{versionregex}$"
-        pattern = re.compile(versiononlyregex)
-        if pattern.match(current_version):
-            GeneralUtilities.write_text_to_file(nuspec_file, re.sub(f"<version>{versionregex}<\\/version>",
-                                                                      f"<version>{current_version}</version>", GeneralUtilities.read_text_from_file(nuspec_file)))
-        else:
-            raise ValueError(f"Version '{current_version}' does not match version-regex '{versiononlyregex}'")
 
-def replace_version_in_csproj_file(self:ScriptCollectionCore,csproj_file:str,current_version:str):
-        versionregex = "\\d+\\.\\d+\\.\\d+"
-        versiononlyregex = f"^{versionregex}$"
-        pattern = re.compile(versiononlyregex)
-        if pattern.match(current_version):
-            GeneralUtilities.write_text_to_file(csproj_file, re.sub(f"<Version>{versionregex}<\\/Version>",
-                                                                      f"<Version>{current_version}</Version>", GeneralUtilities.read_text_from_file(csproj_file)))
-        else:
-            raise ValueError(f"Version '{current_version}' does not match version-regex '{versiononlyregex}'")
+def replace_version_in_nuspec_file(self: ScriptCollectionCore, nuspec_file: str, current_version: str):
+    versionregex = "\\d+\\.\\d+\\.\\d+"
+    versiononlyregex = f"^{versionregex}$"
+    pattern = re.compile(versiononlyregex)
+    if pattern.match(current_version):
+        GeneralUtilities.write_text_to_file(nuspec_file, re.sub(f"<version>{versionregex}<\\/version>",
+                                                                f"<version>{current_version}</version>", GeneralUtilities.read_text_from_file(nuspec_file)))
+    else:
+        raise ValueError(f"Version '{current_version}' does not match version-regex '{versiononlyregex}'")
+
+
+def replace_version_in_csproj_file(self: ScriptCollectionCore, csproj_file: str, current_version: str):
+    versionregex = "\\d+\\.\\d+\\.\\d+"
+    versiononlyregex = f"^{versionregex}$"
+    pattern = re.compile(versiononlyregex)
+    if pattern.match(current_version):
+        GeneralUtilities.write_text_to_file(csproj_file, re.sub(f"<Version>{versionregex}<\\/Version>",
+                                                                f"<Version>{current_version}</Version>", GeneralUtilities.read_text_from_file(csproj_file)))
+    else:
+        raise ValueError(f"Version '{current_version}' does not match version-regex '{versiononlyregex}'")
+
 
 def common_tasks():
-    file=Path(__file__).absolute()
-    folder_of_current_file=os.path.dirname(file)
-    sc=ScriptCollectionCore()
-    version=sc.getversion_from_arguments_or_gitversion(file,sys.argv)
-    sc.update_version_of_codeunit_to_project_version(file,version)
-    replace_version_in_nuspec_file(sc,GeneralUtilities.resolve_relative_path("./Build/GRYLibrary.nuspec",folder_of_current_file),version)
-    replace_version_in_csproj_file(sc,GeneralUtilities.resolve_relative_path("./GRYLibrary/GRYLibrary.csproj",folder_of_current_file),version)
+    file = Path(__file__).absolute()
+    folder_of_current_file = os.path.dirname(file)
+    sc = ScriptCollectionCore()
+    version = sc.getversion_from_arguments_or_gitversion(file, sys.argv)
+    sc.update_version_of_codeunit_to_project_version(file, version)
+    replace_version_in_nuspec_file(sc, GeneralUtilities.resolve_relative_path("./Build/GRYLibrary.nuspec", folder_of_current_file), version)
+    replace_version_in_csproj_file(sc, GeneralUtilities.resolve_relative_path("./GRYLibrary/GRYLibrary.csproj", folder_of_current_file), version)
+
 
 common_tasks()
--- GRYLibrary/Other/Reference/GenerateReference.py
+++ GRYLibrary/Other/Reference/GenerateReference.py
@@ -3,14 +3,18 @@ from pathlib import Path
 from ScriptCollection.ScriptCollectionCore import ScriptCollectionCore
 from ScriptCollection.GeneralUtilities import GeneralUtilities
 
-def standardized_tasks_generate_refefrence_for_dotnet_project_in_common_project_structure(self:ScriptCollectionCore,repository:str):
-    reference_folder=os.path.join(repository,"GRYLibrary","Other","InternalScripts","Reference")
-    reference_result_folder=os.path.join(reference_folder,"Result")
+
+def standardized_tasks_generate_refefrence_for_dotnet_project_in_common_project_structure(self: ScriptCollectionCore, repository: str):
+    reference_folder = os.path.join(repository, "GRYLibrary", "Other", "Reference")
+    reference_result_folder = os.path.join(reference_folder, "Result")
     GeneralUtilities.ensure_directory_does_not_exist(reference_result_folder)
-    self.run_program("docfx","docfx.json",reference_folder)
+    self.run_program("docfx", "docfx.json", reference_folder)
+
 
 def generate_reference():
-    standardized_tasks_generate_refefrence_for_dotnet_project_in_common_project_structure(ScriptCollectionCore(),str(Path(os.path.dirname(__file__)).parent.parent.parent.parent.absolute()))
+    standardized_tasks_generate_refefrence_for_dotnet_project_in_common_project_structure(
+        ScriptCollectionCore(), str(Path(os.path.dirname(__file__)).parent.parent.parent.parent.absolute()))
+
 
 if __name__ == "__main__":
     generate_reference()
