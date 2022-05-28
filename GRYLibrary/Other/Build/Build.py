823110e2d09e5be15ee8d760f99bb19f05ca35c2
Marius Göcke <marius.goecke@gmail.com>
2022-05-28 14:00:33 +0200

    updates;+semver:minor

--- GRYLibrary/Other/Build/Build.py
+++ GRYLibrary/Other/Build/Build.py
@@ -23,8 +23,7 @@ def dotnet_sign_file(self:ScriptCollectionCore,file:str, keyfile:str):
     os.remove(directory+os.path.sep+filename+".il")
     os.remove(directory+os.path.sep+filename+".res")
 
-def standardized_tasks_build_for_dotnet_create_package(self:ScriptCollectionCore,repository:str,codeunitname:str,outputfolder:str,push:bool,push_options:str):
-    #TODO update version in nuspec-file
+def standardized_tasks_build_for_dotnet_create_package(self:ScriptCollectionCore,repository:str,codeunitname:str,outputfolder:str):
     build_folder=os.path.join(repository,codeunitname,"Other","Build")
     root: etree._ElementTree = etree.parse(os.path.join(build_folder, f"{codeunitname}.nuspec"))
     version=root.xpath("//*[name() = 'package']/*[name() = 'metadata']/*[name() = 'version']/text()")[0]
@@ -35,8 +34,6 @@ def standardized_tasks_build_for_dotnet_create_package(self:ScriptCollectionCore
     GeneralUtilities.ensure_directory_does_not_exist(outputfolder)
     GeneralUtilities.ensure_directory_exists(outputfolder)
     os.rename(nupkg_file,f"{build_folder}/BuildArtifact/{nupkg_filename}")
-    if push:
-        pass#TODO push using push_options
 
 def standardized_tasks_build_for_dotnet_build(self:ScriptCollectionCore,csproj_file:str,buildconfiguration:str,outputfolder:str,files_to_sign:dict()):
     #TODO update version in csproj-file
@@ -52,7 +49,7 @@ def standardized_tasks_build_for_dotnet_build(self:ScriptCollectionCore,csproj_f
 def standardized_tasks_build_for_dotnet_executable_project_in_common_project_structure(self:ScriptCollectionCore,repository_folder:str,codeunitname:str,buildconfiguration:str,build_test_project_too:bool,commandline_arguments:list[str]):
     csproj_file=os.path.join(repository_folder,codeunitname,codeunitname,codeunitname+".csproj")
     csproj_test_file=os.path.join(repository_folder,codeunitname,codeunitname+"Tests",codeunitname+"Tests.csproj")
-    outputfolder=os.path.join(os.path.dirname(__file__),"BuildArtifact")
+    outputfolder=os.path.join(repository_folder,codeunitname,"Other","Build","BuildArtifact")
     commandline_arguments=commandline_arguments[1:]
     files_to_sign:dict()=dict()
     for commandline_argument in commandline_arguments:
@@ -68,19 +65,14 @@ def standardized_tasks_build_for_dotnet_library_project_in_common_project_struct
     repository_folder:str=str(Path(os.path.dirname(buildscript_file)).parent.parent.parent.absolute())
     codeunitname:str=os.path.basename( str(Path(os.path.dirname(buildscript_file)).parent.parent.absolute()))
     standardized_tasks_build_for_dotnet_executable_project_in_common_project_structure(self,repository_folder,codeunitname,buildconfiguration,True,commandline_arguments)
-    push=False
-    push_options=""
     for commandline_argument in commandline_arguments:
-        if commandline_argument.startswith("-push:"):
-            push=True
-            push_options=commandline_argument[len("-push:"):]
         if commandline_argument.startswith("-buildconfiguration:"):
             buildconfiguration=commandline_argument[len("-buildconfiguration:"):]
     outputfolder=os.path.join(os.path.dirname(buildscript_file),"BuildArtifact")
-    standardized_tasks_build_for_dotnet_create_package(self,repository_folder,codeunitname,outputfolder,push,push_options)
+    standardized_tasks_build_for_dotnet_create_package(self,repository_folder,codeunitname,outputfolder)
 
 def build():
-    standardized_tasks_build_for_dotnet_library_project_in_common_project_structure(ScriptCollectionCore(),__file__,"QualityCheck", sys.argv)
+    standardized_tasks_build_for_dotnet_library_project_in_common_project_structure(ScriptCollectionCore(),Path(__file__).absolute(),"QualityCheck", sys.argv)
 
 
 if __name__ == "__main__":
