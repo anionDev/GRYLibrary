9f714b4cae2ebf691bd7b3b053b05418572ba217
Marius Göcke <marius.goecke@gmail.com>
2022-10-17 12:43:09 +0200

    updated version

--- GRYLibrary/Other/CommonTasks.py
+++ GRYLibrary/Other/CommonTasks.py
@@ -13,6 +13,7 @@ def common_tasks():
     version = sc.get_semver_version_from_gitversion(GeneralUtilities.resolve_relative_path("../..", os.path.dirname(file)))
     sc.replace_version_in_nuspec_file(GeneralUtilities.resolve_relative_path("./Build/GRYLibrary.nuspec", folder_of_current_file), version)
     sc.replace_version_in_csproj_file(GeneralUtilities.resolve_relative_path("../GRYLibrary/GRYLibrary.csproj", folder_of_current_file), version)
+    sc.replace_version_in_csproj_file(GeneralUtilities.resolve_relative_path("../GRYLibraryTests/GRYLibraryTests.csproj", folder_of_current_file), version)
     tfcps = TasksForCommonProjectStructure()
     tfcps.update_version_of_codeunit_to_project_version(file, version)
 
