f49e7c756f083102d28f6a700c58d40f1a43102a
Marius Göcke <marius.goecke@gmail.com>
2022-10-17 13:53:37 +0200

    updates in scripts and gitignore

--- GRYLibrary/Other/Build/Build.py
+++ GRYLibrary/Other/Build/Build.py
@@ -4,7 +4,8 @@ from ScriptCollection.TasksForCommonProjectStructure import TasksForCommonProjec
 
 
 def build():
-    TasksForCommonProjectStructure().standardized_tasks_build_for_dotnet_library_project_in_common_project_structure(str(Path(__file__).absolute()), "QualityCheck", 1, sys.argv)
+    TasksForCommonProjectStructure().standardized_tasks_build_for_dotnet_library_project_in_common_project_structure(
+        str(Path(__file__).absolute()), "QualityCheck", "QualityCheck", 1, sys.argv)
 
 
 if __name__ == "__main__":
