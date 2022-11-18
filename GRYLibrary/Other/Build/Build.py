18ac87350f7d68eea8829e4b05fa06ceeb802c43
Marius Göcke <marius.goecke@gmail.com>
2022-11-18 16:58:23 +0100

    udpated scripts

--- GRYLibrary/Other/Build/Build.py
+++ GRYLibrary/Other/Build/Build.py
@@ -6,7 +6,7 @@ from ScriptCollection.TasksForCommonProjectStructure import TasksForCommonProjec
 def build():
     t = TasksForCommonProjectStructure()
     t.standardized_tasks_build_for_dotnet_library_project_in_common_project_structure(
-        str(Path(__file__).absolute()), "QualityCheck", t.get_default_build_environment_mapping(), ["win", "linux"], 1, sys.argv)
+        str(Path(__file__).absolute()), "QualityCheck", t.get_default_target_environmenttype_mapping(), ["win", "linux"], 1, sys.argv)
 
 
 if __name__ == "__main__":
--- GRYLibrary/Other/CommonTasks.py
+++ GRYLibrary/Other/CommonTasks.py
@@ -9,9 +9,15 @@ from ScriptCollection.TasksForCommonProjectStructure import TasksForCommonProjec
 def common_tasks():
     file = str(Path(__file__).absolute())
     folder_of_current_file = os.path.dirname(file)
+    cmd_args = sys.argv
     sc = ScriptCollectionCore()
+    t = TasksForCommonProjectStructure()
+    verbosity = t.get_verbosity_from_commandline_arguments(cmd_args, 1)
+    build_environment = t.get_targetenvironmenttype_from_commandline_arguments(cmd_args, "QualityCheck")
+    additional_arguments_file = t.get_additionalargumentsfile_from_commandline_arguments(cmd_args, None)
+    folder_of_current_file = os.path.dirname(file)
     version = sc.get_semver_version_from_gitversion(GeneralUtilities.resolve_relative_path("../..", os.path.dirname(file)))
-    TasksForCommonProjectStructure().update_version_of_codeunit_to_project_version(file, version)
+    t.standardized_tasks_do_common_tasks(file, verbosity, build_environment, True, additional_arguments_file, cmd_args)
     sc.replace_version_in_nuspec_file(GeneralUtilities.resolve_relative_path("./Build/GRYLibrary.nuspec", folder_of_current_file), version)
     sc.replace_version_in_csproj_file(GeneralUtilities.resolve_relative_path("../GRYLibrary/GRYLibrary.csproj", folder_of_current_file), version)
     sc.replace_version_in_csproj_file(GeneralUtilities.resolve_relative_path("../GRYLibraryTests/GRYLibraryTests.csproj", folder_of_current_file), version)
