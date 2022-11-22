c147750fbad52be78c5fd270f7a52510ca47518f
Marius Göcke <marius.goecke@gmail.com>
2022-11-22 11:36:42 +0100

    added generate_sbom_for_dotnet_project

--- GRYLibrary/Other/CommonTasks.py
+++ GRYLibrary/Other/CommonTasks.py
@@ -8,7 +8,6 @@ from ScriptCollection.TasksForCommonProjectStructure import TasksForCommonProjec
 
 def common_tasks():
     file = str(Path(__file__).absolute())
-    folder_of_current_file = os.path.dirname(file)
     cmd_args = sys.argv
     sc = ScriptCollectionCore()
     t = TasksForCommonProjectStructure()
@@ -16,11 +15,13 @@ def common_tasks():
     build_environment = t.get_targetenvironmenttype_from_commandline_arguments(cmd_args, "QualityCheck")
     additional_arguments_file = t.get_additionalargumentsfile_from_commandline_arguments(cmd_args, None)
     folder_of_current_file = os.path.dirname(file)
+    codeunit_folder = GeneralUtilities.resolve_relative_path("..", folder_of_current_file)
     version = sc.get_semver_version_from_gitversion(GeneralUtilities.resolve_relative_path("../..", os.path.dirname(file)))
     t.standardized_tasks_do_common_tasks(file, verbosity, build_environment, True, additional_arguments_file, cmd_args)
     sc.replace_version_in_nuspec_file(GeneralUtilities.resolve_relative_path("./Build/GRYLibrary.nuspec", folder_of_current_file), version)
     sc.replace_version_in_csproj_file(GeneralUtilities.resolve_relative_path("../GRYLibrary/GRYLibrary.csproj", folder_of_current_file), version)
     sc.replace_version_in_csproj_file(GeneralUtilities.resolve_relative_path("../GRYLibraryTests/GRYLibraryTests.csproj", folder_of_current_file), version)
+    t.generate_sbom_for_dotnet_project(codeunit_folder)
 
 
 if __name__ == "__main__":
