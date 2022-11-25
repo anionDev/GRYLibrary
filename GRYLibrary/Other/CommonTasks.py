6e1af9f033618512d94f033b741e9eecb24eb1c2
Marius Göcke <marius.goecke@gmail.com>
2022-11-25 08:46:46 +0100

    updatse

--- GRYLibrary/Other/CommonTasks.py
+++ GRYLibrary/Other/CommonTasks.py
@@ -7,20 +7,22 @@ from ScriptCollection.TasksForCommonProjectStructure import TasksForCommonProjec
 
 
 def common_tasks():
-    file = str(Path(__file__).absolute())
-    folder_of_current_file = os.path.dirname(file)
     cmd_args = sys.argv
-    sc = ScriptCollectionCore()
     t = TasksForCommonProjectStructure()
-    verbosity = t.get_verbosity_from_commandline_arguments(cmd_args, 1)
+    sc = ScriptCollectionCore()
     build_environment = t.get_targetenvironmenttype_from_commandline_arguments(cmd_args, "QualityCheck")
-    additional_arguments_file = t.get_additionalargumentsfile_from_commandline_arguments(cmd_args, None)
+    verbosity = t.get_verbosity_from_commandline_arguments(cmd_args, 1)
+    file = str(Path(__file__).absolute())
+    codeunit_folder = GeneralUtilities.resolve_relative_path("..", os.path.dirname(file))
+    codeunit_name: str = os.path.basename(codeunit_folder)
+    codeunit_version = sc.get_semver_version_from_gitversion(GeneralUtilities.resolve_relative_path(
+        "../..", os.path.dirname(file)))  # Should always be the same as the project-version
     folder_of_current_file = os.path.dirname(file)
-    version = sc.get_semver_version_from_gitversion(GeneralUtilities.resolve_relative_path("../..", os.path.dirname(file)))
-    t.standardized_tasks_do_common_tasks(file, verbosity, build_environment, True, additional_arguments_file, cmd_args)
-    sc.replace_version_in_nuspec_file(GeneralUtilities.resolve_relative_path("./Build/GRYLibrary.nuspec", folder_of_current_file), version)
-    sc.replace_version_in_csproj_file(GeneralUtilities.resolve_relative_path("../GRYLibrary/GRYLibrary.csproj", folder_of_current_file), version)
-    sc.replace_version_in_csproj_file(GeneralUtilities.resolve_relative_path("../GRYLibraryTests/GRYLibraryTests.csproj", folder_of_current_file), version)
+    additional_arguments_file = t.get_additionalargumentsfile_from_commandline_arguments(cmd_args, None)
+    t.standardized_tasks_do_common_tasks(file, codeunit_version, verbosity, build_environment, True, additional_arguments_file, cmd_args)
+    t.replace_common_variables_in_nuspec_file(codeunit_folder)
+    sc.replace_version_in_csproj_file(GeneralUtilities.resolve_relative_path(f"../{codeunit_name}/{codeunit_name}.csproj", folder_of_current_file), codeunit_version)
+    sc.replace_version_in_csproj_file(GeneralUtilities.resolve_relative_path(f"../{codeunit_name}Tests/{codeunit_name}Tests.csproj", folder_of_current_file), codeunit_version)
 
 
 if __name__ == "__main__":
