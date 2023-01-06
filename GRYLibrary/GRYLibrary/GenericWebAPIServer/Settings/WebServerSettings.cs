5dab785a972ab237adfebc2a90118426fb9c0ffa
Marius Göcke <marius.goecke@gmail.com>
2023-01-06 21:20:14 +0100

    added possibility to use https for generic webapi

--- GRYLibrary/GRYLibrary/GenericWebAPIServer/Settings/WebServerSettings.cs
+++ GRYLibrary/GRYLibrary/GenericWebAPIServer/Settings/WebServerSettings.cs
@@ -3,7 +3,9 @@
     public class WebServerSettings
     {
         public string SwaggerDocumentName { get; set; } = "APISpecification";
-        public ushort Port { get; set; } = 4422;
+        public ushort Port { get; set; } = 80;
         public string APIRoutePrefix { get; set; } = "API";
+        public string TLSCertificatePasswordFile { get; set; } = null;
+        public string TLSCertificatePFXFilePath { get; set; } = null;
     }
 }
