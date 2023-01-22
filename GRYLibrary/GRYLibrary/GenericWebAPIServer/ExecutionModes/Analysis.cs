f01122b2f0c9ea4b9fd470e0aefeed9f856c0407
Marius Göcke <marius.goecke@gmail.com>
2023-01-22 13:38:08 +0100

    updates for generic webapiserver

---
+++ GRYLibrary/GRYLibrary/GenericWebAPIServer/ExecutionModes/Analysis.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GRYLibrary.Core.GenericWebAPIServer.ExecutionModes
+{
+    public class Analysis: ExecutionMode
+    {
+        public static Analysis Instance { get; } = new Analysis();
+        private Analysis() { }
+
+        public override void Accept(IExecutionModeVisitor visitor)
+        {
+            visitor.Handle(this);
+        }
+
+        public override T Accept<T>(IExecutionModeVisitor<T> visitor)
+        {
+           return visitor.Handle(this);
+        }
+    }
+}
---
+++ GRYLibrary/GRYLibrary/GenericWebAPIServer/ExecutionModes/ExecutionMode.cs
@@ -0,0 +1,18 @@
+﻿namespace GRYLibrary.Core.GenericWebAPIServer.ExecutionModes
+{
+    public abstract class ExecutionMode
+    {
+        public abstract void Accept(IExecutionModeVisitor visitor);
+        public abstract T Accept<T>(IExecutionModeVisitor<T> visitor);
+    }
+    public interface IExecutionModeVisitor
+    {
+        void Handle(Analysis analysis);
+        void Handle(RunProgram runProgram);
+    }
+    public interface IExecutionModeVisitor<T>
+    {
+        T Handle(Analysis analysis);
+        T Handle(RunProgram runProgram);
+    }
+}
