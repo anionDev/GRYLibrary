7d77656a6b3523ecdd1073a34e72bd23798867ab
Marius Göcke <marius.goecke@gmail.com>
2023-05-04 23:28:08 +0200

    added performance-testkind

--- GRYLibrary/GRYLibrary/GenericWebAPIServer/Middlewares/Configuration/APIKeyValidatorSettings.cs
+++ GRYLibrary/GRYLibrary/GenericWebAPIServer/Middlewares/Configuration/APIKeyValidatorSettings.cs
@@ -8,7 +8,7 @@ namespace GRYLibrary.Core.GenericWebAPIServer.Middlewares.Configuration
         private Func<string/*apiKey*/, string/*method*/, string/*route*/, bool> _APIKeyIsValid { get; set; } = (_, _, _) => true;
         public bool APIKeyIsValid(string apiKey, string method, string route)
         {
-            return _APIKeyIsValid(apiKey, method, route);
+            return this._APIKeyIsValid(apiKey, method, route);
         }
         public APIKeyValidatorSettings(Func<string/*apiKey*/, string/*method*/, string/*route*/, bool> apiKeyIsValid)
         {
