010a7e1ff085c1e93ca69ac21514a6c6c9b24fe9
Marius Göcke <marius.goecke@gmail.com>
2023-05-05 12:23:32 +0200

    added UnorderedList

---
+++ GRYLibrary/GRYLibrary/Miscellaneous/UnorderedList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GRYLibrary.Core.Miscellaneous
+{
+    public class UnorderedList<T> :IEnumerable<T>, IEquatable<UnorderedList<T>>
+    {
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Equals(object other)
+        {
+            return Equals(other as UnorderedList<T>);
+        }
+
+        public override int GetHashCode()
+        {
+            return 42;//TODO
+        }
+        /// <returns>Returns true if and only if there is a bujection between this and <paramref name="other"/>.</returns>
+        public bool Equals(UnorderedList<T> other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
