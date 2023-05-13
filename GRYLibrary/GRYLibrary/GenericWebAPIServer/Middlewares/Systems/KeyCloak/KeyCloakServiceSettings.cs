d17616e29711520d02577af333585c4ded772012
Marius Göcke <marius.goecke@gmail.com>
2023-05-14 01:10:00 +0200

    updates in keycloaksettings

--- GRYLibrary/GRYLibrary/GenericWebAPIServer/Middlewares/Systems/KeyCloak/KeyCloakServiceSettings.cs
+++ GRYLibrary/GRYLibrary/GenericWebAPIServer/Middlewares/Systems/KeyCloak/KeyCloakServiceSettings.cs
@@ -3,6 +3,7 @@
     public class KeyCloakServiceSettings:IKeyCloakServiceSettings
     {
         public string URL { get; set; }
+        public string Realm { get; set; }
         public string User { get; set; }
         public string Password { get; set; }
     }
