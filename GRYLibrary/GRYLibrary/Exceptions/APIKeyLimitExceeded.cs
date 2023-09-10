f68b7288ae0aad08251141f42f4209250435ecd6
Marius Göcke <marius.goecke@gmail.com>
2023-09-10 16:48:50 +0200

    added some exception-types

---
+++ GRYLibrary/GRYLibrary/Exceptions/APIKeyLimitExceeded.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GRYLibrary.Core.Exceptions
+{
+    public class APIKeyLimitExceeded : Exception
+    {
+        public APIKeyLimitExceeded() : this("API-Key limit exceeded.")
+        {
+        }
+        public APIKeyLimitExceeded(string message) : base(message)
+        {
+        }
+    }
+}
