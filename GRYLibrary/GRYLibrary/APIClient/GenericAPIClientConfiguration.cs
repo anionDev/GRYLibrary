f6904b4ae2da357686a655af4bb5aa2ab3062055
Marius Göcke <marius.goecke@gmail.com>
2023-10-03 01:54:42 +0200

    added generic apiclient

---
+++ GRYLibrary/GRYLibrary/APIClient/GenericAPIClientConfiguration.cs
@@ -0,0 +1,9 @@
+﻿
+namespace GRYLibrary.Core.GenericAPIClient
+{
+    public class GenericAPIClientConfiguration
+    {
+        public string APIKey { get; set; }
+        public string APIAddress { get; set; }
+    }
+}
---
+++ GRYLibrary/GRYLibrary/APIClient/IGenericAPIClientConfiguration.cs
@@ -0,0 +1,9 @@
+﻿
+namespace GRYLibrary.Core.GenericAPIClient
+{
+    public interface IGenericAPIClientConfiguration
+    {
+        public string APIKey { get; set; }
+        public string APIAddress { get; set; }
+    }
+}
