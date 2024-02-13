d47dad554e8f2af5db4916794606a52000baa22c
Marius Göcke <marius.goecke@gmail.com>
2024-02-13 07:46:59 +0100

    added AuthorizationTokenCredentialsProvider

---
+++ GRYLibrary/GRYLibrary/APIServer/Services/Trans/AuthorizationTokenCredentialsProvider.cs
@@ -0,0 +1,23 @@
+﻿using GRYLibrary.Core.APIServer.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GRYLibrary.Core.APIServer.Services.Trans
+{
+    public class AuthorizationTokenCredentialsProvider : IHTTPCredentialsProvider
+    {
+        public bool ContainsCredentials(HttpContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ExtractSecret(HttpContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
