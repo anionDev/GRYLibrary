e3afd4abd7a3b0a8082e404ac584962811796442
Marius Göcke <marius.goecke@gmail.com>
2024-02-14 00:39:31 +0100

    little updates in BadRequestException

--- GRYLibrary/GRYLibrary/Exceptions/BadRequestException.cs
+++ GRYLibrary/GRYLibrary/Exceptions/BadRequestException.cs
@@ -1,27 +1,27 @@
-﻿using System.Net;
+﻿using Microsoft.AspNetCore.Http;
 using GUtilities = GRYLibrary.Core.Miscellaneous.Utilities;
 
 namespace GRYLibrary.Core.Exceptions
 {
     public class BadRequestException : UserFormattedException
     {
-        public uint HTTPStatusCode { get; }
-        public BadRequestException(uint httpStatusCode) : this(httpStatusCode, GetMessage(httpStatusCode))
+        public ushort HTTPStatusCode { get; }
+        public BadRequestException(ushort httpStatusCode) : this(httpStatusCode, GetMessage(httpStatusCode))
         {
         }
-        public BadRequestException(uint httpStatusCode, string message) : base(message)
+        public BadRequestException(ushort httpStatusCode, string message) : base(message)
         {
-            GUtilities.AssertCondition((httpStatusCode / 100)==4);
+            GUtilities.AssertCondition((httpStatusCode / 100) == 4);
             this.HTTPStatusCode = httpStatusCode;
         }
 
-        private static string GetMessage(uint httpStatusCode)
+        private static string GetMessage(ushort httpStatusCode)
         {
-            if (httpStatusCode == (int)HttpStatusCode.Unauthorized)
+            if (httpStatusCode == StatusCodes.Status401Unauthorized)
             {
                 return "Authentication required";
             }
-            else if (httpStatusCode == (int)HttpStatusCode.Forbidden)
+            else if (httpStatusCode == StatusCodes.Status403Forbidden)
             {
                 return "Unauthorized";
             }
