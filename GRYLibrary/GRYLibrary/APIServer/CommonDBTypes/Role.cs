1570241efbe0a32d51f150d3b7450eec0ccb778f
Marius Göcke <marius.goecke@gmail.com>
2024-04-09 23:32:43 +0200

    added roles as db-type; workaround to prevent high cpu usages in loops

---
+++ GRYLibrary/GRYLibrary/APIServer/CommonDBTypes/Role.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace GRYLibrary.Core.APIServer.CommonDBTypes
+{
+    [PrimaryKey(nameof(Id))]
+    public class Role : IEquatable<Role>
+    {
+        public string Id { get; set; }
+        public string Name { get; set; } = null;
+
+        public override bool Equals(object obj)
+        {
+            return this.Equals(obj as User);
+        }
+
+        public bool Equals(Role other)
+        {
+            return other is not null &&
+                   this.Id == other.Id &&
+                   this.Name == other.Name;
+        }
+
+        public override int GetHashCode()
+        {
+            HashCode hash = new HashCode();
+            hash.Add(this.Id);
+            return hash.ToHashCode();
+        }
+    }
+}
