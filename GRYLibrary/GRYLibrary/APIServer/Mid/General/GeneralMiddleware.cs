3f1ea4ff76a957477ea51baa16da9ebb783d4bee
Marius Göcke <marius.goecke@gmail.com>
2024-10-13 15:48:56 +0200

    added temporary log in general middleware

--- GRYLibrary/GRYLibrary/APIServer/Mid/General/GeneralMiddleware.cs
+++ GRYLibrary/GRYLibrary/APIServer/Mid/General/GeneralMiddleware.cs
@@ -1,5 +1,6 @@
 ﻿using GRYLibrary.Core.APIServer.MidT.General;
 using GRYLibrary.Core.APIServer.Settings.Configuration;
+using GRYLibrary.Core.Logging.GRYLogger;
 using Microsoft.AspNetCore.Http;
 using System;
 using System.Net;
@@ -10,9 +11,11 @@ namespace GRYLibrary.Core.APIServer.Mid.General
     public class GeneralMiddleware<PersistedApplicationSpecificConfiguration> : GeneralMiddlewareT
         where PersistedApplicationSpecificConfiguration : new()
     {
+        private readonly IGRYLog _Log;
         private readonly IPersistedAPIServerConfiguration<PersistedApplicationSpecificConfiguration> _PersistedAPIServerConfiguration;
-        public GeneralMiddleware(RequestDelegate next, IPersistedAPIServerConfiguration<PersistedApplicationSpecificConfiguration> persistedAPIServerConfiguration) : base(next)
+        public GeneralMiddleware(RequestDelegate next, IGRYLog log, IPersistedAPIServerConfiguration<PersistedApplicationSpecificConfiguration> persistedAPIServerConfiguration) : base(next)
         {
+            _Log = log;
             this._PersistedAPIServerConfiguration = persistedAPIServerConfiguration;
         }
 
@@ -29,6 +32,7 @@ namespace GRYLibrary.Core.APIServer.Mid.General
             {
                 if (context.Request.Headers.TryGetValue("X-Forwarded-For", out Microsoft.Extensions.Primitives.StringValues value) && (value != default(string)))
                 {
+                    _Log.Log($"Retrieved IP by X-Forwarded-For-header which changed the result-ip-address from {result} to '{value}'.",Microsoft.Extensions.Logging.LogLevel.Debug);
                     result = IPAddress.Parse((string)value!);
                 }
             }
