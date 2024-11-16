5a4861ae9581bb8a6641a7310d114b1f1dd700f0
Marius Göcke <marius.goecke@gmail.com>
2024-11-16 22:29:02 +0100

    added AbstractSQLProvider

---
+++ GRYLibrary/GRYLibrary/Misc/AbstractSQLProvider.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace GRYLibrary.Core.Misc
+{
+    public class AbstractSQLProvider
+    {
+        private readonly string _SQLFilesNamespace;
+        protected AbstractSQLProvider(string sqlFilesNamespace) {
+            _SQLFilesNamespace=sqlFilesNamespace;
+        }
+        private IDictionary<string, string> _ScriptCache = new Dictionary<string, string>();
+
+        protected string LoadSQLScript(string sqlFileName)
+        {
+            if (!this._ScriptCache.ContainsKey(sqlFileName))
+            {
+                this.LoadScriptToCache(sqlFileName);
+            }
+            return this._ScriptCache[sqlFileName];
+        }
+
+        private void LoadScriptToCache(string sqlFileName)
+        {
+            using (Stream stream = this.GetType().Assembly.GetManifestResourceStream($"{_SQLFilesNamespace}.{sqlFileName}.sql"))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                this._ScriptCache[sqlFileName] = reader.ReadToEnd();
+            }
+        }
+    }
+}
