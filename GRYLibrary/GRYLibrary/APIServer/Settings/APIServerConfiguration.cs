360a222c508d309e27cc4817481f3a0da81025bd
Marius Göcke <marius.goecke@gmail.com>
2025-01-04 02:30:57 +0100

    added apiServerConfiguration.CommandlineParameter

--- GRYLibrary/GRYLibrary/APIServer/Settings/APIServerConfiguration.cs
+++ GRYLibrary/GRYLibrary/APIServer/Settings/APIServerConfiguration.cs
@@ -5,11 +5,16 @@ namespace GRYLibrary.Core.APIServer.Settings
     public class APIServerConfiguration<ApplicationSpecificConstants, PersistedApplicationSpecificConfiguration, CommandlineParameterType>
         where PersistedApplicationSpecificConfiguration : new()
     {
+        public APIServerConfiguration()
+        {
+
+        }
         internal InitializationInformation<ApplicationSpecificConstants, PersistedApplicationSpecificConfiguration, CommandlineParameterType> InitializationInformation { get; set; }
         public Action<InitializationInformation<ApplicationSpecificConstants, PersistedApplicationSpecificConfiguration, CommandlineParameterType>> SetInitialzationInformationAction { get; set; } = (_) => { };
         internal FunctionalInformation<ApplicationSpecificConstants, PersistedApplicationSpecificConfiguration, CommandlineParameterType> FunctionalInformation { get; set; }
         public Action<FunctionalInformation<ApplicationSpecificConstants, PersistedApplicationSpecificConfiguration, CommandlineParameterType>> SetFunctionalInformationAction { get; set; } = (_) => { };
         internal FunctionalInformationForWebApplication<ApplicationSpecificConstants, PersistedApplicationSpecificConfiguration, CommandlineParameterType> FunctionalInformationForWebApplication { get; set; }
         public Action<FunctionalInformationForWebApplication<ApplicationSpecificConstants, PersistedApplicationSpecificConfiguration, CommandlineParameterType>> ConfigureWebApplication { get; set; } = (_) => { };
+        public CommandlineParameterType CommandlineParameter { get; set; }
     }
 }
