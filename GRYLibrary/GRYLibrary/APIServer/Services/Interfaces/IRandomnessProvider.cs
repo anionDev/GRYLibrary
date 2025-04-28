b9cda0514ced0712329b4375abca4552bec1ac47
Marius Göcke <marius.goecke@gmail.com>
2025-04-28 17:58:13 +0200

    added SecureRandomnessProvider

--- GRYLibrary/GRYLibrary/APIServer/Services/Interfaces/IRandomnessProvider.cs
+++ GRYLibrary/GRYLibrary/APIServer/Services/Interfaces/IRandomnessProvider.cs
@@ -1,9 +1,15 @@
 ﻿namespace GRYLibrary.Core.APIServer.Services.Interfaces
 {
+    /// <summary>
+    /// Represents a randomness provider.
+    /// </summary>
     public interface IRandomnessProvider
     {
         /// <returns>Returns a non-negative number which is lower than <paramref name="maximalValue"/>.</returns>
         public int Next(int maximalValue);
+        // <summary>
+        // Fills the array with random bytes.
+        // </summary>
         void NextBytes(byte[] buffer);
     }
 }
