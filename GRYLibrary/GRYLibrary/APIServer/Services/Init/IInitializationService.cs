7cbddd1d677f534bb8e97e4efad990ec14c4795a
Marius Göcke <marius.goecke@gmail.com>
2025-08-28 23:39:25 +0200

    Added Initialization-state.

--- GRYLibrary/GRYLibrary/APIServer/Services/Init/IInitializationService.cs
+++ GRYLibrary/GRYLibrary/APIServer/Services/Init/IInitializationService.cs
@@ -1,13 +1,19 @@
-﻿using GRYLibrary.Core.Misc.ConsoleApplication;
+﻿using GRYLibrary.Core.APIServer.Utilities;
+using GRYLibrary.Core.Misc.ConsoleApplication;
 
 namespace GRYLibrary.Core.APIServer.Services.Init
 {
+    public interface IInitializationService
+    {
+        public InitializationState GetInitializationState();
+    }
     /// <summary>
     /// Does business-logic-related initialization.
     /// </summary>
-    public interface IInitializationService<CommandlineParameter>
+    public interface IInitializationService<CommandlineParameter>: IInitializationService
         where CommandlineParameter : ICommandlineParameter
     {
         public void Initialize(CommandlineParameter commandlineParameter);
+        public InitializationState GetInitializationState();
     }
 }
