44d46a2a8bace4c6b62e9958417f9696e1f64f2b
Marius Göcke <marius.goecke@gmail.com>
2025-09-20 11:26:15 +0200

    added constructor overloading for ServiceNotAvailableException

--- GRYLibrary/GRYLibrary/Exceptions/ServiceNotAvailableException.cs
+++ GRYLibrary/GRYLibrary/Exceptions/ServiceNotAvailableException.cs
@@ -4,6 +4,12 @@ namespace GRYLibrary.Core.Exceptions
 {
     public class ServiceNotAvailableException : Exception
     {
+        public ServiceNotAvailableException() : base("Service currently not available.")
+        {
+        }
+        public ServiceNotAvailableException(string message) : base(message)
+        {
+        }
         public ServiceNotAvailableException(string message, Exception innerException) : base(message, innerException)
         {
         }
