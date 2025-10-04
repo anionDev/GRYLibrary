ac0b30a0d08d12b0ed9a60b467f8c6c66570c793
Marius Göcke <marius.goecke@gmail.com>
2025-10-04 13:16:15 +0200

    update in buildscript

--- GRYLibrary/Other/CommonTasks.py
+++ GRYLibrary/Other/CommonTasks.py
@@ -4,7 +4,7 @@ from ScriptCollection.TFCPS.DotNet.CertificateGeneratorInformationNoGenerate imp
  
 def common_tasks():
     tf:TFCPS_CodeUnitSpecific_DotNet_Functions=TFCPS_CodeUnitSpecific_DotNet_CLI.parse(__file__)
-    tf.do_common_tasks(tf.get_version_of_project(),CertificateGeneratorInformationNoGenerate())#codeunit-version should alsways be the same as project-version
+    tf.do_common_tasks(tf.get_version_of_project(),CertificateGeneratorInformationNoGenerate(),False)#codeunit-version should alsways be the same as project-version
 
 
 if __name__ == "__main__":
