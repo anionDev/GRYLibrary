1e1a0cf5e4b5f595bb1982ab094545d503bca9fe
Marius Göcke <marius.goecke@gmail.com>
2025-10-05 13:39:08 +0200

    fix for datetimeoffset in cookieservice

--- GRYLibrary/GRYLibrary/APIServer/Services/CredC/CookieTools.cs
+++ GRYLibrary/GRYLibrary/APIServer/Services/CredC/CookieTools.cs
@@ -6,17 +6,17 @@ namespace GRYLibrary.Core.APIServer.Services.CredC
     public static class CookieTools
     {
         public static string CookieName { get; set; } = "X-Authorization";
-        public static (string key, string value, CookieOptions options) GetAccessTokenCookie(string username, string accessToken, DateTime expires)
+        public static (string key, string value, CookieOptions options) GetAccessTokenCookie(string username, string accessToken, DateTimeOffset expires)
         {
             return GetCookieWithSpecificExpiredDate(username, expires, accessToken);
         }
 
         public static (string key, string value, CookieOptions options) GetAccessTokenExpiredCookie(string username)
         {
-            return GetCookieWithSpecificExpiredDate(username, new DateTime(1970, 1, 1, 0, 0, 0), string.Empty);
+            return GetCookieWithSpecificExpiredDate(username, new DateTimeOffset(1970, 1, 1, 0, 0, 0,TimeSpan.Zero), string.Empty);
         }
 
-        public static (string key, string value, CookieOptions options) GetCookieWithSpecificExpiredDate(string username, DateTime expiredDate, string accessToken)
+        public static (string key, string value, CookieOptions options) GetCookieWithSpecificExpiredDate(string username, DateTimeOffset expiredDate, string accessToken)
         {
             return (CookieName,
                 $"User={username};AccessToken={accessToken}",
